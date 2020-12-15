1d0f7311f4099f8468c22f7c26175a0734091707
Richard Williams <rwill128.create@gmail.com>
2020-12-14 19:21:21 -0500

    Initial commit

---
+++ textile_classification.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import wandb
+from wandb.integration.tensorflow import WandbHook
+
+
+wandb.login()
+wandb.init(config=tf.python.flags.FLAGS, project="tensorflow-examples", entity='rwill128')
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+
+history = model.fit(train_images, train_labels, epochs=10)
+
+print(history)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
